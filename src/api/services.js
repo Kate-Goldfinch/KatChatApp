9847431c2d192ac5b41df36526921ff2e40a42bb
Kate Goldfinch <kate.a.goldfinch@gmail.com>
2023-08-08 14:16:36 +1000

    fixed api url

--- src/api/services.js
+++ src/api/services.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const baseURL = 'https://comp3120-chat.herokuapp.com/'
-const baseURL = "https://katchatapp.onrender.com/"
+const baseURL = "https://katchatapp-api.onrender.com"
 
 const createUser = (user) => {
   const request = axios.post(baseURL + "auth/register", user);
