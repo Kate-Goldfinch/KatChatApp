24759290e2cf8f335e2477c6ac1e4ca03272a4e8
Kate Goldfinch <kate.a.goldfinch@gmail.com>
2023-08-08 14:55:06 +1000

    cors pls

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const errorhandler = require('errorhandler')
 const cookieParser = require('cookie-parser')
 const notifier = require('node-notifier')
+const cors = require('cors')
 
 const router = require('./routes')
 const config = require('./config')
@@ -19,22 +20,14 @@ function errorNotification (err, str, req) {
   }
 
 const app = express()
-app.use(function(req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
-  const allowedOrigins = ['http://localhost:3000', 'http://katchatapp.onrender.com', 'https://katchatapp.onrender.com'];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-       res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-  next();
-});
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.static('build'))
-
+app.use(cors({
+  credentials: true,
+  origin: "https://katchatapp.onrender.com"
+}))
+app.options('*', cors())
 if (process.env.NODE_ENV === 'development') {
     // only use in development
     app.use(errorhandler({ log: errorNotification }))
