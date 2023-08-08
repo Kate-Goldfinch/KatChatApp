f4ff56c87b358ed711a539599eb7f8bbcb56ad92
Kate Goldfinch <kate.a.goldfinch@gmail.com>
2023-08-08 14:32:41 +1000

    fixing cors error

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,6 @@ const morgan = require('morgan')
 const errorhandler = require('errorhandler')
 const cookieParser = require('cookie-parser')
 const notifier = require('node-notifier')
-const cors = require('cors')
 
 const router = require('./routes')
 const config = require('./config')
@@ -23,10 +22,18 @@ const app = express()
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.static('build'))
-app.use(cors({
-  credentials: true,
-  origin: config.corsClientDomain
-}))
+app.use(function(req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = ['http://localhost:3000', 'http://katchatapp.onrender.com', 'https://katchatapp.onrender.com'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
 if (process.env.NODE_ENV === 'development') {
     // only use in development
     app.use(errorhandler({ log: errorNotification }))
